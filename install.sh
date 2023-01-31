07a6aa2b3bd754e6b752084f3789bfb348e32fcc
AlexEbrim <123337068+AlexEbrim@users.noreply.github.com>
2023-01-31 19:42:34 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,25 +6,52 @@ sudo apt-get update
 sudo apt-get -y install nginx-full 
 
 filename=/etc/nginx/sites-available/default;
-
+echo -n "Please enter your domain: "
+read configSSLDomain
 
 cat > /etc/nginx/sites-available/default <<-EOF
 
 server {
-        listen 40512 default_server;
-        listen [::]:40512 default_server;
+        listen 8443 ssl http2;
+        listen [::]:8443 http2;
+
+        ssl_certificate       /etc/letsencrypt/live/$configSSLDomain/fullchain.pem;
+        ssl_certificate_key   /etc/letsencrypt/live/$configSSLDomain/privkey.pem;
+        ssl_protocols         TLSv1.2 TLSv1.3;
+        ssl_ciphers           TLS-AES-256-GCM-SHA384:TLS-CHACHA20-POLY1305-SHA256:TLS-AES-128-GCM-SHA256:TLS-AES-128-CCM-8-SHA256:TLS-AES-128-CCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256;
+
+        ssl_stapling on;
+        ssl_stapling_verify on;
+        add_header Strict-Transport-Security "max-age=31536000";
 
         root /var/www/html;
-
-        
         index index.html index.htm index.nginx-debian.html;
 
-        server_name _;
+        server_name $configSSLDomain;
 
 
         location / {
                 try_files \$uri \$uri.html \$uri/ @extensionless-php;
                 index index.html index.htm index.php;
+        }
+
+		location /delaservice {
+            grpc_pass grpc://127.0.0.1:2096;
+            grpc_connect_timeout 60s;
+            grpc_read_timeout 720m;
+            grpc_send_timeout 720m;
+            grpc_set_header X-Real-IP \$remote_addr;
+            grpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        }
+
+         location /delaweb {
+            proxy_pass http://127.0.0.1:2098;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$http_host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         }
 
         location ~ \.php\$ {
@@ -39,6 +66,8 @@ server {
         location @extensionless-php {
                 rewrite ^(.*)\$ \$1.php last;
         }
+		
+		
 }
 
 EOF
@@ -51,10 +80,17 @@ sudo apt-get install -y php7.4-fpm php7.4-cli php7.4-json php7.4-common php7.4-m
 
 sudo ufw disable
 
+
+
+sudo apt install snapd
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email dzasc@gmail.com -d $configSSLDomain
+
 sudo systemctl daemon-reload
 sudo systemctl enable nginx
 sudo systemctl restart nginx
-
 
 sudo apt-get -y install unzip
 
