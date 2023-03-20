9a58f4bda02e421220a1cbbac48db2a15c95d91c
AlexEbrim <123337068+AlexEbrim@users.noreply.github.com>
2023-03-20 22:27:01 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,7 @@ sudo apt-get install -y npm
 sudo npm install -g npm@latest
 sudo apt-get -y install nginx-full 
 
-sudo apt -y install ufw
-sudo ufw allow 56777
-sudo ufw allow 443
-sudo ufw allow 8080
-sudo ufw allow 80
-sudo ufw enable
+sudo apt -y install ufw && sudo ufw allow 56777 && sudo ufw allow 443 && sudo ufw allow 8080 && sudo ufw allow 80 && sudo ufw enable
 
 filename=/etc/nginx/sites-available/default;
 echo -n "Please enter your domain:"
