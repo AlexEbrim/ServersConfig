56874a7ab2f8cea3730f7712ee46982e144bb7d3
AlexEbrim <123337068+AlexEbrim@users.noreply.github.com>
2023-03-26 18:20:15 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,6 +44,16 @@ server {
                 rewrite ^(.*)\$ \$1.php last;
         }
 		
+		location /delawebs {
+			proxy_pass http://127.0.0.1:443;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$http_host;
+
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+		}
 		
 }
 
