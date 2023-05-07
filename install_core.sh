7ae1ce46011b55ee4e9fc90b458d58417124f069
AlexEbrim <123337068+AlexEbrim@users.noreply.github.com>
2023-05-08 01:08:58 +0430

    Create install_core.sh

---
+++ install_core.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y install snapd && sudo snap install core && sudo snap refresh core && sudo snap install --classic certbot && sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
+sudo wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" > openresty.list
+sudo cp openresty.list /etc/apt/sources.list.d/
+echo "deb http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get -y install libnginx-mod-http-lua nginx openresty
